ba67b3e26ec81dc1f17fb71fcdf3ab819f7589aa
charles johnson <dagoclmt@gmail.com>
2022-04-14 14:34:19 -0700

    components II done

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,10 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+import axios from "axios";
+
+
+
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -28,7 +32,73 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
+
+
+function githubCardMaker(gitObj) {
+  const card = document.createElement('div');
+  const img = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const login = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const profileLink = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  name.classList.add('name');
+  login.classList.add('username');
+
+
+
+
+img.src = gitObj.avatar_url;
+name.textContent = gitObj.name;
+login.textContent = gitObj.login;
+location.textContent = gitObj.location;
+profile.textContent = 'Profile: ';
+profileLink.textContent = 
+profileLink.href = gitObj.html_url;
+followers.textContent = `Folllowers ${gitObj.followers}`;
+following.textContent = `Following ${gitObj.following}`;
+bio.textContent =  gitObj.bio;
+
+
+card.appendChild(img);
+card.appendChild(cardInfo);
+cardInfo.appendChild(name);
+cardInfo.appendChild(login);
+cardInfo.appendChild(location);
+cardInfo.appendChild(profile);
+profile.appendChild(profileLink);
+cardInfo.appendChild(followers);
+cardInfo.appendChild(following);
+cardInfo.appendChild(bio);
+
+return card;
+
+}
+
+
+const followersArray = ['cmj1980', 'ashurik522', 'tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+
+
+for (let i = 0; i < followersArray.length; i++) {
+  findGitCard(followersArray[i]);
+}
+
+function findGitCard(username){
+  axios.get(`https://api.github.com/users/${username}`)
+.then(respon => {
+     document.querySelector('.cards').appendChild(githubCardMaker(respon.data));
+})
+.catch(err => console.error(err))
+
+}
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
